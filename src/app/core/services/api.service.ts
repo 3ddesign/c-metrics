3cf932c9000e56befda7f27cd89877acfaf47e45
Dmitry <3ddesign@bk.ru>
2020-10-04 09:33:03 +0300

    add base methods

--- src/app/core/services/api.service.ts
+++ src/app/core/services/api.service.ts
@@ -1,9 +1,28 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { environment } from '../../../environments/environment';
+import { Observable, of } from 'rxjs';
+import { share, catchError } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApiService {
 
-  constructor() { }
+  constructor( private http: HttpClient) { }
+
+  public get(path: string, params?: any): Observable<any> {
+    return this.http
+      .get<any>(`${environment}${path}`, { params })
+      .pipe(
+        share(),
+        catchError((error: any) => {
+          return this.handleError(error);
+        })
+      );
+  }
+
+  private handleError(error: any) {
+    return of(error);
+  }
 }
