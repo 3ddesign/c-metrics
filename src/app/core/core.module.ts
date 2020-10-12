27c1d62f2dabca0a6abb982e7a6b74f30209aafa
Dmitry <3ddesign@bk.ru>
2020-10-12 09:17:55 +0300

    add header

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { MenuComponent } from './components/menu/menu.component';
+import { MatCardModule } from '@angular/material/card';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 @NgModule({
   declarations: [DashboardComponent, MenuComponent],
   imports: [
+    MatToolbarModule,
+    MatCardModule,
     CommonModule
   ]
 })
