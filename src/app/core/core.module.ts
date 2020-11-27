76a5ed0c701945f8ad4d16687af33e4390fff627
Dmitry <3ddesign@bk.ru>
2020-11-27 13:49:58 +0200

    add new routes

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,16 +1,18 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { DashboardComponent } from './components/dashboard/dashboard.component';
-import { MenuComponent } from './components/menu/menu.component';
-import { MatToolbarModule } from '@angular/material/toolbar';
+import { NgModule } from '@angular/core';
+
 import { MatMenuModule } from '@angular/material/menu';
 import { MatSnackBarModule } from '@angular/material/snack-bar';
- 
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { RouterModule } from '@angular/router';
 import { SharedModule } from '../shared/shared.module';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { MenuComponent } from './components/menu/menu.component';
 
 @NgModule({
   declarations: [DashboardComponent, MenuComponent],
   imports: [
+    RouterModule,
     SharedModule,
     MatMenuModule,
     MatSnackBarModule,
