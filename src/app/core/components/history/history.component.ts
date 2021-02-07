f2967457a9a52507858ee3bfcfa09c836bf53d46
Dmitry <3ddesign@bk.ru>
2021-02-08 00:43:05 +0200

    get date range

--- src/app/core/components/history/history.component.ts
+++ src/app/core/components/history/history.component.ts
@@ -1,7 +1,8 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { SubSink } from 'subsink';
 import { ApiService } from '../../services/api.service';
 import * as mockData from '../mock-data/report';
-
+import { DateTime } from 'luxon';
 @Component({
   selector: 'app-history',
   templateUrl: './history.component.html',
@@ -12,9 +13,16 @@ export class HistoryComponent {
   dataSource = mockData.REPORT_DATA;
   historyData: string[] = ['date', 'currency', 'value'];
 
-  constructor(public apiService: ApiService) {}
+  private subs = new SubSink();
+
+  constructor(public apiService: ApiService) {
+    this.onLoadHisotry();
+  }
 
   onLoadHisotry(): void {
-    // this.apiService.getCurrencyHistory().subscribe();
+    this.subs.add(
+    this.apiService.getCurrencyHistory(`/api/v7/convert?q=USD_UAH&compact=ultra&date=${DateTime.local().minus({ days: 3 }).toISODate()}&endDate=${DateTime.local().toISODate()}`).subscribe(currencyHistory => {
+      console.log(currencyHistory);
+    }));
   }
 }
