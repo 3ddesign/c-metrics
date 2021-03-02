70cd5fbed73f3679b37f86ff02bd1c77394f78b6
Dmitry <3ddesign@bk.ru>
2021-03-02 08:46:36 +0200

    add budget module

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,6 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 
+import { MatButtonToggleModule } from '@angular/material/button-toggle';
 import { MatSnackBarModule } from '@angular/material/snack-bar';
 import { MatTableModule } from '@angular/material/table';
 
@@ -16,6 +17,7 @@ import { BudgetComponent } from './components/budget/budget.component';
     SharedModule,
     MatTableModule,
     MatSnackBarModule,
+    MatButtonToggleModule,
     CommonModule
   ]
 })
