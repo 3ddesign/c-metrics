6a3cfa234f84c03ac074a3427531766d5c4f8b2e
Dmitry <3ddesign@bk.ru>
2021-03-10 08:49:40 +0200

    add converter controls

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -2,8 +2,10 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 
 import { MatButtonToggleModule } from '@angular/material/button-toggle';
+import { MatButtonModule } from '@angular/material/button';
 import { MatSnackBarModule } from '@angular/material/snack-bar';
 import { MatTableModule } from '@angular/material/table';
+import { MatInputModule } from '@angular/material/input';
 
 import { SharedModule } from '../shared/shared.module';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
@@ -15,6 +17,8 @@ import { ConverterComponent } from './components/converter/converter.component';
   declarations: [DashboardComponent, HistoryComponent, ConverterComponent],
   imports: [
     SharedModule,
+    MatButtonModule,
+    MatInputModule,
     MatTableModule,
     MatSnackBarModule,
     MatButtonToggleModule,
