8d0c95f0f6a6080bad1cd4c2abb2bf6bc5878cd3
Dmitry <3ddesign@bk.ru>
2021-03-14 07:23:16 +0200

    style changes

--- src/app/core/components/converter/converter.component.ts
+++ src/app/core/components/converter/converter.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ApiService } from '../../services/api.service';
 
 @Component({
   selector: 'cmx-converter',
@@ -7,6 +8,12 @@ import { Component } from '@angular/core';
 })
 export class ConverterComponent {
 
+  constructor(private apiService: ApiService) {}
+
   public convertedValue!: number;
+
+  onContertValue() {
+
+  }
 
 }
