cb223ae1695462da4e56a15f0aff514dca52b3ba
Dmitry <3ddesign@bk.ru>
2021-03-30 09:52:21 +0300

    test formatting

--- src/app/shared/components/menu/menu.component.ts
+++ src/app/shared/components/menu/menu.component.ts
@@ -1,12 +1,11 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 
 @Component({
-  selector: 'cmx-menu',
-  templateUrl: './menu.component.html',
-  styleUrls: ['./menu.component.scss'],
-  changeDetection: ChangeDetectionStrategy.OnPush
+    selector: 'cmx-menu',
+    templateUrl: './menu.component.html',
+    styleUrls: ['./menu.component.scss'],
+    changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class MenuComponent {
-
-  constructor() { }
+    constructor() {}
 }
