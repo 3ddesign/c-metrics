2c9857f8302ee0fe8eb0211416cf6787718cdbfd
Dmitry <3ddesign@bk.ru>
2021-04-02 09:06:38 +0300

    add formatting

--- src/app/core/components/converter/converter.component.ts
+++ src/app/core/components/converter/converter.component.ts
@@ -1,18 +1,16 @@
 import { Component } from '@angular/core';
-import { StateService } from '../../services/state.service';
 
 @Component({
-  selector: 'cmx-converter',
-  templateUrl: './converter.component.html',
-  styleUrls: ['./converter.component.scss']
+    selector: 'cmx-converter',
+    templateUrl: './converter.component.html',
+    styleUrls: ['./converter.component.scss']
 })
 export class ConverterComponent {
+    constructor() {}
 
-  constructor(private stateService: StateService) {}
+    public convertedValue!: number;
 
-  public convertedValue!: number;
-
-  onContertValue(value: string): void {
-    console.log(value);
-  }
+    onContertValue(value: string): void {
+        console.log(value);
+    }
 }
