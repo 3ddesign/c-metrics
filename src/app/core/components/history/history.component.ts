9f70e5483be795ddd4b4a655c27dac4901a707da
Dmitry <3ddesign@bk.ru>
2021-04-07 09:21:22 +0300

    remove code duplication

--- src/app/core/components/history/history.component.ts
+++ src/app/core/components/history/history.component.ts
@@ -3,36 +3,47 @@ import { SubSink } from 'subsink';
 import { ApiService } from '../../services/api.service';
 import { DateTime } from 'luxon';
 @Component({
-  selector: 'cmx-history',
-  templateUrl: './history.component.html',
-  styleUrls: ['./history.component.scss'],
-  changeDetection: ChangeDetectionStrategy.Default,
+    selector: 'cmx-history',
+    templateUrl: './history.component.html',
+    styleUrls: ['./history.component.scss'],
+    changeDetection: ChangeDetectionStrategy.Default
 })
 export class HistoryComponent implements OnDestroy {
-  dataSource!: { date: string, currency: string, value: number }[];
-  historyData: string[] = ['date', 'currency', 'value'];
-  isLoadingContent = true;
+    dataSource!: { date: string; currency: string; value: number }[];
+    historyData: string[] = ['date', 'currency', 'value'];
+    isLoadingContent = true;
 
-  private subs = new SubSink();
-  private daysAmount: { days: number } = { days: 8 };
-  private mainCurrencyPair = 'USD_UAH';
+    private subs = new SubSink();
+    private daysAmount: { days: number } = { days: 8 };
+    private mainCurrencyPair = 'USD_UAH';
 
-  constructor(public apiService: ApiService) {
-    this.onLoadHisotry();
-  }
+    constructor(public apiService: ApiService) {
+        this.onLoadHisotry();
+    }
 
-  onLoadHisotry(): void {
-    this.subs.add(
-    this.apiService.getCurrencyHistory(`/api/v7/convert?q=USD_UAH&compact=ultra&date=${DateTime.local().minus(this.daysAmount).toISODate()}&endDate=${DateTime.local().toISODate()}`).subscribe((currencyHistory: any) => {
-      this.dataSource = [];
-      for (const date in currencyHistory[this.mainCurrencyPair]) {
-        this.dataSource.push({ date: date, currency: 'USD/UAH', value: currencyHistory[this.mainCurrencyPair][date] });
-      }
-      this.isLoadingContent = false;
-    }));
-  }
+    onLoadHisotry(): void {
+        this.subs.add(
+            this.apiService
+                .getCurrency(
+                    `/api/v7/convert?q=USD_UAH&compact=ultra&date=${DateTime.local()
+                        .minus(this.daysAmount)
+                        .toISODate()}&endDate=${DateTime.local().toISODate()}`
+                )
+                .subscribe((currencyHistory: any) => {
+                    this.dataSource = [];
+                    for (const date in currencyHistory[this.mainCurrencyPair]) {
+                        this.dataSource.push({
+                            date: date,
+                            currency: 'USD/UAH',
+                            value: currencyHistory[this.mainCurrencyPair][date]
+                        });
+                    }
+                    this.isLoadingContent = false;
+                })
+        );
+    }
 
-  ngOnDestroy() {
-    this.subs.unsubscribe();
-  }
+    ngOnDestroy() {
+        this.subs.unsubscribe();
+    }
 }
--- src/app/core/services/api.service.ts
+++ src/app/core/services/api.service.ts
@@ -6,40 +6,29 @@ import { share, catchError } from 'rxjs/operators';
 import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Injectable({
-  providedIn: 'root'
+    providedIn: 'root'
 })
 export class ApiService {
-  public isApplicationOnline!: boolean;
-  constructor(private http: HttpClient, private snackBar: MatSnackBar) { }
+    public isApplicationOnline!: boolean;
+    constructor(private http: HttpClient, private snackBar: MatSnackBar) {}
 
-  public getCurrency(path: string, params?: any): Observable<any> {
-    return this.http
-      .get<any>(`${environment.currencyApi}${path}&apiKey=${environment.accessKey}`, { params })
-      .pipe(
-        share(),
-        catchError((error: any) => {
-          return this.handleError(error);
-        })
-      );
-  }
-
-  public getCurrencyHistory(path: string, params?: any): Observable<any> {
-    return this.http
-      .get<any>(`${environment.currencyApi}${path}&apiKey=${environment.accessKey}`, { params })
-      .pipe(
-        share(),
-        catchError((error: any) => {
-          return this.handleError(error);
-        })
-      );
-  }
-
-  private handleError(error: any): Observable<any> {
-    if (this.isApplicationOnline) {
-      this.snackBar.open('Http Error', error?.statusText, {
-        duration: 4000,
-      });
+    public getCurrency(path: string, params?: any): Observable<any> {
+        return this.http
+            .get<any>(`${environment.currencyApi}${path}&apiKey=${environment.accessKey}`, { params })
+            .pipe(
+                share(),
+                catchError((error: any) => {
+                    return this.handleError(error);
+                })
+            );
     }
-    return of(error);
-  }
+
+    private handleError(error: any): Observable<any> {
+        if (this.isApplicationOnline) {
+            this.snackBar.open('Http Error', error?.statusText, {
+                duration: 4000
+            });
+        }
+        return of(error);
+    }
 }
