78b3bf8460b657bca4e8512cae82034a60211f14
Dmitry <3ddesign@bk.ru>
2022-10-07 09:23:56 +0200

    update accessKey

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
-  production: true,
-  isOffileMode: false,
-  currencyApi: 'https://free.currconv.com',
-  accessKey: '15718ccc26ece1b807b0'
+    production: true,
+    isOffileMode: false,
+    currencyApi: 'https://free.currconv.com',
+    accessKey: '8da7a3c7b21f01e994ca'
 };
